89904d521adfc2f8c84a50405c49beee014e6561
Iryna M <iirinaaryna@gmail.com>
2023-02-13 23:07:11 +0300

    feat:complete task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+  if (n<0) n=n*(-1)
+  let i=0;
+  let result = '';
+  let stringNum = String(n);
+  while(i<stringNum.length){
+    result = `${stringNum[i]}${result}`;
+    i =i+1;
+  }
+return Number(result);
+};
